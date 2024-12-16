0e04eb6609b2fd9d6e5a992efd5bcb750def2ff4
bird-ui <ethanvoboril@seattleacademy.org>
2024-12-16 20:16:41 +0000

    rainbow six siege

---
+++ Projects/project2.py
@@ -0,0 +1,32 @@
+defense_points = 0
+attack_points = 0
+
+answer = input("do you like playing A( play more slow, or B) play more fast?\n")
+if answer == "A":
+    defense_points += 1
+elif answer == "B":
+    attack_points += 1
+
+answer = input("do you like playing A( play with cooler gadgets, or B) play with more destructive gadgets?\n")
+if answer == "A":
+    defense_points += 1
+elif answer == "B":
+    attack_points += 1
+
+answer = input("do you like playing A( use cameras , or B) use drones?\n")
+if answer == "A":
+    defense_points += 1
+elif answer == "B":
+    attack_points += 1
+
+answer = input("do you like playing A( more heals, or B) more damage output?\n")
+if answer == "A":
+    defense_points += 1
+elif answer == "B":
+    attack_points += 1
+
+answer = input("do you like playing A( weaker weapons and better gadgets, or B) better weapons but weaker gadgets?\n")
+if answer == "A":
+    defense_points += 1
+elif answer == "B":
+    attack_points += 1
